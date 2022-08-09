f4b0229c11d15772d11b145a8d107da62b3e5f9d
Ayyan <ayyanbabar11@gmail.com>
2022-08-09 10:11:02 +0500

    Making the UI of the calculator and starting to add functionality using reducers and dispatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
+import { useReducer } from "react";
+import DigitButton from "./DigitButton";
+import "./styles.css";
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete_digit",
+  EVALUATE: "evaluate",
+};
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        curr: `${curr || ""}${payload.digit}`,
+      };
+  }
+}
 function App() {
-  //TEST
-  return null;
+  const [{ curr, prev, op }, dispatch] = useReducer(reducer, {});
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-op">
+          {prev} {op}
+        </div>
+        <div className="current-op">{curr}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <DigitButton digit="÷" dispatch={dispatch}>
+        ÷
+      </DigitButton>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
 }
 
 export default App;
